b25dacbc74e07ec86971c6012c0d40f64ca57332
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2019-04-18 17:54:38 -0300

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+function botao(num){
+    var salvo =  document.calc.visor.value;
+    document.calc.visor.value=salvo+num;
+}
+
+function reset(limpa){
+    var resultado =  document.calc.visor.value;
+    document.calc.visor.value ='';
+    
+    
+
+}
+
+function calcule(){   
+    var resultado =  document.calc.visor.value;
+    var calculo = eval(resultado);
+    document.calc.visor.value=calculo;
+    
+    }
+    
+    
+    
+   
+    
+
+    
